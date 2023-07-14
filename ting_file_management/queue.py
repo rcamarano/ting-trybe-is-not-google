3dd2b50b2c01884ca6e1ff4140cacd84ebb061b1
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 17:43:35 -0300

    req 1 create reading queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,15 +4,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 class Queue(AbstractQueue):
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self.list.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.list.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index >= len(self.list):
+            raise IndexError("Index inválido ou Inexistente")
