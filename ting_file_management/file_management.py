0d3aab0e9b5bc1ffc9bba931557dda58ea61db84
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 18:21:29 -0300

    req 2, txt_importer implemented, passing on local tests

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return []
+    try:
+        with open(path_file, 'r', encoding='utf+8') as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
