23b8a65faedab99cb44724c75a89edb96a49a4b9
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 14:15:13 -0300

    starting the project

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,5 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    """Aqui irá sua implementação, vamos começar com a implementação mais sim"""
 
 
 def search_by_word(word, instance):
