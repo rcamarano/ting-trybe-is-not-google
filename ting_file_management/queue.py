a3447f4e18d45f0a0996b4e054c4b2032de297e9
rcamarano <rogeriocamarano@gmail.com>
2023-07-14 19:33:29 -0300

    req 6 started, still with some problemas to fix

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,22 +4,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 class Queue(AbstractQueue):
     def __init__(self):
         """Inicialize sua estrutura aqui"""
-        self.list = list()
+        self._list = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
-        return len(self.list)
+        return len(self._list)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
-        return self.list.append(value)
+        return self._list.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
-        return self.list.pop(0)
+        if len(self._list) == 0:
+            return None
+        else:
+            return self._list.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
-        if index < 0 or index >= len(self.list):
+        if index < 0 or index >= len(self._list):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.list[index]
+        return self._list[index]
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação, vamos começar"""
+    files_base = [
+        {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": line}
+                for line, line_content in enumerate(
+                    file["linhas_do_arquivo"], start=1
+                )
+                if word.lower() in line_content.lower()
+            ],
+        }
+        for file in instance._list
+        if any(
+            word.lower() in line_content.lower()
+            for line_content in file["linhas_do_arquivo"]
+        )
+    ]
+
+    return files_base
 
 
 def search_by_word(word, instance):
