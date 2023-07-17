9d9a3ac3e45675c36d90e12f03c06e601c0f4ffb
rcamarano <rogeriocamarano@gmail.com>
2023-07-17 18:12:17 -0300

    req 8 serach_by_word mplemented, passing on local tests

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,3 +24,25 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação, VQV!"""
+    words_foud = []
+
+    for file_content in instance._list:
+        file_name = file_content["nome_do_arquivo"]
+        times_apeared = []
+
+        for line_number, line_content in enumerate(
+            file_content["linhas_do_arquivo"], start=1
+        ):
+            if word.lower() in line_content.lower():
+                incidence = {"linha": line_number, "conteudo": line_content}
+                times_apeared.append(incidence)
+
+        if times_apeared:
+            result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": times_apeared,
+            }
+            words_foud.append(result)
+
+    return words_foud
